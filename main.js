a659a287c5e8bbf901fb465ff3b898a4e0a60a9b
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-05-24 11:03:56 -0500

    adding usb monitor

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function createWindow() {
   }));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (config.showDevTools) mainWindow.webContents.openDevTools();
 
   mainWindow.webContents.session.clearCache(function () {
     //some callback.
